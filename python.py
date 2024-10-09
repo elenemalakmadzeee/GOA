227265d4a5df2153a253d2028e6a5a85e04b8994
elenemalakmadzeee <elenemal3@gmail.com>
2024-10-09 19:02:45 +0400

    first commit

---
+++ python.py
@@ -0,0 +1,32 @@
+mom="shorena"
+
+print(mom)
+
+dad="vaja"
+
+
+print(dad)
+
+print(mom,dad)
+
+print("elene malakmadze")
+
+me="elene"
+
+print(me)
+
+print("ana malakmadze")
+
+sister="ana"
+
+print(sister)
+
+print(me,sister)
+
+
+
+
+
+
+
+
