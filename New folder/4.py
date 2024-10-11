aab1a48d47315a8d027681c13a5ce9b7de9e43c8
elenemalakmadzeee <elenemal3@gmail.com>
2024-10-11 15:38:30 +0400

    second commit

---
+++ New folder/4.py
@@ -0,0 +1,9 @@
+#შევქმნათ ორი ცვლადი
+name=("elene")
+lastname=("malakmadze")
+#მოვახდინოთ ამ ცვლადების კონკადინაცია
+print("elene"+"malakmadze")
+
+result=("elenemalakmadze")
+
+
---
+++ New folder/5.py
@@ -0,0 +1,5 @@
+#შევქმნათ ორი ცვლადი
+age=(13)
+fullname=("elene malakmadze")
+#დავპრინტოთ
+print(fullname*age)
