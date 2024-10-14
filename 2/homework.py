18143edcd549d99a98ab40aa4e5e06a8ab4235f9
elenemalakmadzeee <elenemal3@gmail.com>
2024-10-14 18:18:16 +0400

    second commit

---
+++ 2/homework.py
@@ -0,0 +1,21 @@
+
+ # შევქმნათ ცვლადი შემოვიტანოთ :
+#სახელი
+name=input(" your name is : ")
+print(" your  name is "+ str(name ))
+# გვარი
+lastname=input(" enter your lastname  : ")
+print("your lastname is "+str(lastname ))
+#ასაკი
+age=input(" enter your age  : ")
+print("your age is  "+str(age ))
+#მისამართი
+address=input("enter your address  : ")
+print(" your adress is "+str(address ))
+#ემაილი
+email=input(" enter your email  : ")
+print("your email is "+str(email ))
+
+
+
+
