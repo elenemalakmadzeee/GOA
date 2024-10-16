f5856d01aee6c22eaed9b2019df756a9b64bdb88
elenemalakmadzeee <elenemal3@gmail.com>
2024-10-16 20:16:05 +0400

    second commit

---
+++ lvl6/3.py
@@ -0,0 +1,7 @@
+#მომხარებელს შემოატანინეთ თავისი ასაკი, თუ ის არის 18 წლის ან ზემოთ დაპრინტეთ "You are adult", სხვა შემთხვევაში დაპრინტეთ "You are kid"
+age=int(input ("enter your age : "))
+if age >=18: 
+  print ("you are adult")
+else: print ("you are kid")
+
+
---
+++ lvl6/5.py
@@ -0,0 +1,11 @@
+#გააკეთეთ 3-3 მაგალითი int() და str() ფუნქციებზე
+
+
+a=int("5")
+b=int("12")
+c=int("45")
+print(a+b+c)
+d=str(5)
+e=str(98)
+f=str(4)
+print(d+e+f)
