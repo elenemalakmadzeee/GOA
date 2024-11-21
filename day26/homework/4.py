458cde18a5c126a4a1c63bde46b92357abb71e2d
qweqwe1qq <qweqwe@gmail.com>
2024-11-21 20:04:16 +0400

    16th commit

---
+++ day26/homework/4.py
@@ -0,0 +1,7 @@
+# შექმენით ფუნქცია, რომელიც იღებს წინადადებას, და მასში space-ების მაგივრად სიტყვებს 
+# შორის ჩასვამს ტირეს("-"). საბოლოოდ კი აბრუნებს მას
+
+def space_in_sentence(text):
+    cleaned_sentence = "-".join(text.split())
+    return cleaned_sentence
+
